fafd2a99957516a416fc34cd9aa90505d49576a8
Ankit Kumar <ankitkumar@Ankits-MacBook-Air.local>
2025-09-22 21:05:25 +0530

    added deployment code.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/data-scientist-portfolio/',
+  build: {
+    outDir: 'dist'
+  },
   optimizeDeps: {
     include: ['@mui/material', '@emotion/react', '@emotion/styled']
   },
